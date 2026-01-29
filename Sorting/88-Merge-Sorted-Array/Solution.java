7a7115077ce30affa60e5a88f1abf4ead57b36d9
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2026-01-29 19:10:20 +0530

    Create Solution.java

---
+++ Sorting/88-Merge-Sorted-Array/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(n == 0) return;
+         int index1 = m-1;
+         int index2 = n-1;
+         int i = nums1.length - 1;
+         while(index2 >= 0){
+            if(index1 >= 0 && nums1[index1] > nums2[index2]){
+                nums1[i] = nums1[index1];
+                index1--;
+            } else {
+                nums1[i] = nums2[index2];
+                index2--;
+            }
+            i--;
+         }
+    }
+}

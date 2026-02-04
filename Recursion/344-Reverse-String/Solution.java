a12f07e1ef7a6ac623860bf0c52362f6d6538ef7
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2026-02-04 10:01:43 +0530

    Create Solution.java

---
+++ Recursion/344-Reverse-String/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+        reverse(s, start, end);
+    }
+    void reverse(char[] s, int start, int end){
+        if(start > end || start == end) return;
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        reverse(s, start + 1, end - 1);
+
+    }
+}

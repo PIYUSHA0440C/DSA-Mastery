a70b0633d4688ba2c6c76f7bbedea4d365db7017
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2026-03-12 10:37:46 +0530

    Implement reverseVowels method in Solution class

---
+++ Strings/345-Reverse-Vowels-of-a-String/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String reverseVowels(String s)
+    {
+        char[] arr= s.toCharArray();
+        int left=0;
+        int right=arr.length-1;
+        String vowels="aeiouAEIOU";
+        while(left<right)
+        {
+            if(vowels.indexOf(arr[left])==-1)
+            {
+                left++;
+            }
+            else if(vowels.indexOf(arr[right])==-1)
+            {
+                right--;
+            }
+            else
+            {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return new String(arr);
+    }
+}

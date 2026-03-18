b5b332754ba42141ec87773b5d482baa0b49c72d
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2026-03-18 20:54:27 +0530

    Implement method to find largest odd number in string

---
+++ Strings/1903-Largest-Odd-Number-in-String/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        int len = num.length() - 1;
+        while(len >= 0){
+            if((num.charAt(len) - '0') % 2 != 0){
+                return num.substring(0, len + 1);
+            }
+            len--;
+        }
+        return "";
+    }
+}

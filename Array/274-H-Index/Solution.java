94947fb9da3943747dc797442509f8425bbd330a
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2026-04-05 22:00:54 +0530

    Create Solution.java

---
+++ Array/274-H-Index/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int h = 0;
+        for(int i = citations.length - 1; i >= 0; i--){
+            if(citations[i] > h) h++;
+            else break;
+        }
+
+        return h;
+    }
+}

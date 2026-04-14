2797063069ba65f5f470d5889bee9766120a9c54
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2026-04-14 21:55:02 +0530

    Create Solution.java

---
+++ Bit Manipulation - Maths/070-Climbing-Stairs/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n < 3) return n;
+
+        int n1 = 1, n2 = 2;
+        while( n - 2 > 0){
+            n2 = n2 + n1;
+            n1 = n2 - n1;
+            n--;
+        }
+
+        return n2;
+    }
+}

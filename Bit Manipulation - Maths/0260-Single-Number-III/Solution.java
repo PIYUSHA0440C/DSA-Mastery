5e60f25bb13ee8b8ea5f2a6da136a83644145343
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2026-05-30 20:28:18 +0530

    Create Solution.java

---
+++ Bit Manipulation - Maths/0260-Single-Number-III/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xorAll = 0;
+
+        for(int num : nums) xorAll ^= num;
+
+        int diffBit = xorAll & (-xorAll);
+
+        int x = 0;
+        for(int num : nums){
+            if((diffBit & num) != 0) x ^= num;
+        }
+
+        return new int[]{x, xorAll ^ x};
+
+    }
+}

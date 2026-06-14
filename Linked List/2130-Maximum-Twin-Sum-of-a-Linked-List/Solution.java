1d1d40db61226abfd781788a452e20e7ab2aa1c3
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2026-06-14 23:23:45 +0530

    Create Solution.java

---
+++ Linked List/2130-Maximum-Twin-Sum-of-a-Linked-List/Solution.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        int sum = 0;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        ListNode curr = slow;
+        while(curr != null){
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        slow = head;
+        while(prev != null){
+            sum = Math.max(sum, slow.val + prev.val);
+            slow = slow.next;
+            prev = prev.next;
+        }
+
+        return sum;
+
+    }
+}

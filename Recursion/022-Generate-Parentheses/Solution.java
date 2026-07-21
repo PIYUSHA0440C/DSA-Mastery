48432704da6b0ba039caff97afc4911e15185cf8
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2026-07-21 22:46:58 +0530

    Update Solution.java

--- Recursion/022-Generate-Parentheses/Solution.java
+++ Recursion/022-Generate-Parentheses/Solution.java
@@ -1,21 +1,40 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> list = new ArrayList<>();
-        helper(list, "", 0, 0, n);
-        return list;
+        List<String> combinations = new ArrayList<>();
+        generate(combinations, "", 0, 0, n);
+
+        return combinations;
     }
-    
-    void helper(List<String> list, String s, int open, int close, int max){
-        if(s.length() == max * 2){
-            list.add(s);
+
+    private void generate(List<String> combinations, String str, int open, int close, int max) {
+        if(close == max) {
+            combinations.add(str);
             return;
         }
-        
-        if(open < max){
-            helper(list, s + "(", open + 1, close, max);
-        }
-        if(close < open){
-            helper(list, s + ")", open, close + 1, max);
-        }
+
+        if(open < max) generate(combinations, str + "(", open + 1, close, max);
+        if(close < open) generate(combinations, str + ")", open, close + 1, max);
     }
 }
+
+// class Solution {
+//     public List<String> generateParenthesis(int n) {
+//         List<String> list = new ArrayList<>();
+//         helper(list, "", 0, 0, n);
+//         return list;
+//     }
+    
+//     void helper(List<String> list, String s, int open, int close, int max){
+//         if(s.length() == max * 2){
+//             list.add(s);
+//             return;
+//         }
+        
+//         if(open < max){
+//             helper(list, s + "(", open + 1, close, max);
+//         }
+//         if(close < open){
+//             helper(list, s + ")", open, close + 1, max);
+//         }
+//     }
+// }

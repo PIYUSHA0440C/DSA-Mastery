853dbeee5e3c7210a7407b149c308195605c6941
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2026-08-13 21:45:28 +0530

    Create Solution.java

---
+++ Binary Tree/1161-maximum-level-sum-of-a-binary-tree/Solution.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        
+        int maxSum = Integer.MIN_VALUE;
+        int maxLevel = 1;
+        int currentLevel = 1;
+
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+            int levelSum = 0;
+
+            for(int i = 0; i < levelSize; i++){
+                TreeNode node = queue.poll();
+                levelSum += node.val;
+
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+
+            if(levelSum > maxSum) {
+                maxSum = levelSum;
+                maxLevel = currentLevel;
+            }
+
+            currentLevel++;
+        }
+
+        return maxLevel;
+    }
+}

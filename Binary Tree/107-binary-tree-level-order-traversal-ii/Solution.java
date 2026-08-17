78ba5536fac2bb7af5d4c753dee65ea22308f17d
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2026-08-17 21:58:31 +0530

    Create Solution.java

---
+++ Binary Tree/107-binary-tree-level-order-traversal-ii/Solution.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if(root == null) return ans;
+
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int n = queue.size();
+            List<Integer> nodes = new ArrayList<>();
+
+            for(int i = 0; i < n; i++){
+                TreeNode node = queue.poll();
+                nodes.add(node.val);
+
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+
+            ans.add(nodes);
+        }
+
+        Collections.reverse(ans);
+
+        return ans;
+    }
+}

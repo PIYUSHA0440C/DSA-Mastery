b7ac37d797544eff76f7647b4d88e90254184cd7
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2026-09-26 21:50:59 +0530

    Create Solution.java

---
+++ Sliding Window & Two Pointer/1679-Max-Number-of-K-Sum-Pairs/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int left = 0;
+        int right = nums.length - 1;
+        int operations = 0;
+
+        while(left < right){
+            int currentSum = nums[left] + nums[right];
+
+            if (currentSum == k){
+                operations++;
+                left++;
+                right--;
+            } else if (currentSum < k){
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return operations;
+    }
+}
